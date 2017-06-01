ebc03190d0cf6b436ed1ee3ef362eefc5f16480a
Keiwen <romainwald@hotmail.com>
2017-06-01 18:01:25 +0200

    fix issue with data set (string instead of int)

--- src/store/modules/serie.js
+++ src/store/modules/serie.js
@@ -126,15 +126,15 @@ const mutations = {
     if (typeof data.highest === 'undefined') data.highest = data.rank
 
     // rank between 0 and 25
-    if (data.rank >= 0 && data.rank <= 25) state.rank = data.rank
+    if (data.rank >= 0 && data.rank <= 25) state.rank = parseInt(data.rank)
     // stars between 0 and max by rank, 0 if rank 0
     if (state.rank === 0) data.stars = 0
-    if (data.stars >= 0 && data.stars <= state.RANKS[state.rank]['stars']) state.stars = data.stars
+    if (data.stars >= 0 && data.stars <= state.RANKS[state.rank]['stars']) state.stars = parseInt(data.stars)
     // win streak must be > 0
-    if (data.winStreak >= 0) state.winStreak = data.winStreak
+    if (data.winStreak >= 0) state.winStreak = parseInt(data.winStreak)
     // highest should be at least rank
     if (data.highest > state.rank) data.highest = state.rank
-    if (data.highest >= 0 && data.highest <= 25) state.highest = data.highest
+    if (data.highest >= 0 && data.highest <= 25) state.highest = parseInt(data.highest)
   },
   [types.INCREASE_RANK] (state) {
     if (state.rank === 0) return // max level
