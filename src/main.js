8f1d8272cffc66dbb742d969e013f87310cfe37a
Keiwen <romainwald@hotmail.com>
2017-06-28 11:44:46 +0200

    add welcoming message with toastr

--- src/main.js
+++ src/main.js
@@ -10,15 +10,6 @@ import VueAnalytics from 'vue-analytics'
 import 'vue-awesome/icons'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'chart.js'
-// TODO rework toastr
-/*
-I dont like s4l1h/vue-toastr because of its ugly/heavy usage like this.$root.$refs.toastr.e("ERRROR MESSAGE");
-cxlt-vue2-toastr is better with its this.$toast.success({})
-Anyway, these libs doesnt include all feature initially in codeseven jquery lib
-Maybe we could fork this and complete it (check first one too)
-btw, this.$toast is defined in index/main js of the src, like Vue.prototype.$toast
-Note that cxlt include a lot of nice animation but on in, message can flash after animation before timeout
-*/
 import CxltToastr from 'cxlt-vue2-toastr'
 import 'cxlt-vue2-toastr/dist/css/cxlt-vue2-toastr.css'
 
@@ -37,6 +28,7 @@ Vue.use(VueAnalytics, {
 const toastrConfig = {
   position: 'top right',
   timeOut: 2000,
+  progressBar: true,
   hideDuration: 500,
   closeButton: false,
   hideMethod: 'rotateOutUpRight'
@@ -50,5 +42,11 @@ new Vue({
   store,
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  mounted () {
+    this.$toast.info({
+      title: 'Hello',
+      message: 'Welcome to Trackerstone'
+    })
+  }
 })
