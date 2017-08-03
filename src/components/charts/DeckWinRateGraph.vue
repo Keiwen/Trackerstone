86ec7f34d04cfefce8aa50105f6bd4d3bbad8b6d
Keiwen <romainwald@hotmail.com>
2017-08-03 09:03:59 +0200

    use store getters to compute winrates

--- src/components/charts/DeckWinRateGraph.vue
+++ src/components/charts/DeckWinRateGraph.vue
@@ -26,24 +26,8 @@
           const played = gamesList.length
           if (played === 0) continue
           labels.push(this.getDeckTitle(deck))
-          const recentGamesList = this.$store.getters.getGamesWithDeck(idDeck, true)
-          const recentPlayed = recentGamesList.length
-          let winPercent = 0
-          let recentWinPercent = 0
-          if (played !== 0) {
-            const won = gamesList.filter(game => {
-              return game['won'] === true
-            }).length
-            winPercent = Math.round((won / played) * 100)
-          }
-          if (recentPlayed !== 0) {
-            const recentWon = recentGamesList.filter(game => {
-              return game['won'] === true
-            }).length
-            recentWinPercent = Math.round((recentWon / recentPlayed) * 100)
-          }
-          datasetGlobal.data.push(winPercent)
-          datasetRecent.data.push(recentWinPercent)
+          datasetGlobal.data.push(this.$store.getters.getWinPercentWithDeck(idDeck))
+          datasetRecent.data.push(this.$store.getters.getWinPercentWithDeck(idDeck, true))
         }
 
         return { labels: labels, datasets: [datasetGlobal, datasetRecent] }
