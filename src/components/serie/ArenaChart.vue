6d717cbfc3249f55da7d37d66680c7f2daf763a4
Keiwen <romainwald@hotmail.com>
2017-09-05 12:14:51 +0200

    add arena gold evolution graph

--- src/components/serie/ArenaChart.vue
+++ src/components/serie/ArenaChart.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="container">
         <div class="chart">
-            <h2>Win in completed arena</h2>
-            <arena-won-evol-graph :height="100" />
-        </div>
-        <div class="chart">
             <h2>Class played against</h2>
             <class-against-arena-graph :height="100" />
+        </div>
+        <div class="chart">
+            <h2>Gold prize evolution</h2>
+            <arena-gold-evol-graph :height="100" />
+        </div>
+        <div class="chart">
+            <h2>Win in completed arena</h2>
+            <arena-won-evol-graph :height="100" />
         </div>
 
     </div>
@@ -17,9 +21,10 @@
 
   import ClassAgainstArenaGraph from '@/components/charts/ClassAgainstArenaGraph'
   import ArenaWonEvolGraph from '@/components/charts/ArenaWonEvolGraph'
+  import ArenaGoldEvolGraph from '@/components/charts/ArenaGoldEvolGraph'
 
   export default {
-    components: {ClassAgainstArenaGraph, ArenaWonEvolGraph}
+    components: {ClassAgainstArenaGraph, ArenaWonEvolGraph, ArenaGoldEvolGraph}
   }
 
 </script>
