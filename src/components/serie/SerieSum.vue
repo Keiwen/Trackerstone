551c0a065d87ee129c045e9c18b9332e4f18b1f4
Keiwen <romainwald@hotmail.com>
2017-09-06 14:52:20 +0200

    add progress bar in serie

--- src/components/serie/SerieSum.vue
+++ src/components/serie/SerieSum.vue
@@ -3,18 +3,31 @@
         <h2>Serie</h2>
         <p>Rank: {{ rank }} - {{ currentRankTitle }} // Stars: {{ stars }} (Chest: {{ highest }})</p>
         <p>Next milestone: {{ nextMilestone}} - {{ milestoneTitle }} ({{ winsToMilestone }} wins needed)</p>
+        <div class="serieTime">
+            <radial-progress-bar diameter="100"
+                                 :completed-steps="getSerieTimeProgress"
+                                 strokeWidth="10"
+                                 total-steps="100"
+                                 startColor="#463C3B"
+                                 stopColor="#D3921F"
+                                 innerStrokeColor="#DDDDDD">
+                <p>{{ getSerieTimeLeft }}<br/>days left</p>
+            </radial-progress-bar>
+        </div>
     </div>
 </template>
 
 
 <script>
   import { mapGetters } from 'vuex'
+  import RadialProgressBar from 'vue-radial-progress'
 
   export default {
+    components: { RadialProgressBar },
     computed: {
       ...mapGetters(['rank', 'stars', 'highest', 'nextMilestone', 'winsToMilestone',
         'gamesPlayed', 'gamesWon', 'winRate', 'current', 'opponent', 'recentNumberGames',
-        'rankTitle']),
+        'rankTitle', 'getSerieTimeProgress', 'getSerieTimeLeft']),
       currentRankTitle () {
         return this.rankTitle()
       },
@@ -25,3 +38,11 @@
   }
 
 </script>
+
+
+<style>
+    .radial-progress-container {
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
