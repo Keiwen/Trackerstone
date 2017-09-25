48d5ce0940d984fa7f7524f9f72ab840ca755ad5
Keiwen <romainwald@hotmail.com>
2017-09-25 11:53:52 +0200

    hard reset button

--- src/components/settings/Settings.vue
+++ src/components/settings/Settings.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="container-fluid">
         <serie-set></serie-set>
+        <hr/>
+
+        <confirmation-modal
+                @modal-confirm="hardReset()"
+                modalText="You will loose all your data. Are you sure to continue?"
+                buttonText="Reset to default data" iconName=""
+        />
+
     </div>
 </template>
 
@@ -8,9 +16,15 @@
 <script>
 
   import SerieSet from '@/components/serie/SerieSet'
+  import ConfirmationModal from '@/components/modals/ConfirmationModal'
 
   export default {
-    components: {SerieSet}
+    components: {SerieSet, ConfirmationModal},
+    methods: {
+      hardReset () {
+        this.$store.dispatch('resetState')
+      }
+    }
   }
 
 </script>
