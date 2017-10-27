bbb533c90e36bf6e39b47b03fd483987b91b56ef
Keiwen <keiwen.mk@gmail.com>
2017-10-27 16:19:58 +0200

    new graph: winpercent by hour

---
+++ src/components/charts/WinPercentByHourGraph.vue
@@ -0,0 +1,82 @@
+
+<script>
+
+  import { Bubble } from 'vue-chartjs'
+  import { mapGetters } from 'vuex'
+
+  export default Bubble.extend({
+    props: {
+      weekend: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      getEmptyPoint (i) {
+        return {
+          gameCount: 0,
+          winCount: 0,
+          x: i,
+          y: 0,
+          r: 0
+        }
+      },
+      getPointToDisplay (pointData) {
+        if (!pointData.gameCount) return pointData
+        const winPercent = Math.round(pointData.winCount / pointData.gameCount * 100)
+        pointData.r = pointData.gameCount
+        pointData.y = winPercent
+        return pointData
+      }
+    },
+    computed: {
+      ...mapGetters(['getGamesList']),
+      chartData () {
+        const history = this.getGamesList()
+        let hours = []
+        let hoursWeekend = []
+        for (let i = 0; i < 24; i++) {
+          hours.push(this.getEmptyPoint(i))
+          hoursWeekend.push(this.getEmptyPoint(i))
+        }
+
+        // split history in hour range
+        for (let i = 0; i < history.length; i++) {
+          if (!history[i].gameDate) continue
+          const dayInWeek = history[i].gameDate.dayInWeek
+          if (dayInWeek === 0 || dayInWeek === 6) {
+            hoursWeekend[history[i].gameDate.hour].gameCount++
+            if (history[i].won) hoursWeekend[history[i].gameDate.hour].winCount++
+          } else {
+            hours[history[i].gameDate.hour].gameCount++
+            if (history[i].won) hours[history[i].gameDate.hour].winCount++
+          }
+        }
+
+        for (let i = 0; i < 24; i++) {
+          hours[i] = this.getPointToDisplay(hours[i])
+          hoursWeekend[i] = this.getPointToDisplay(hoursWeekend[i])
+        }
+
+        return {
+          datasets: [
+            {
+              label: 'Week',
+              data: hours,
+              backgroundColor: '#D3921F'
+            },
+            {
+              label: 'Weekend',
+              data: hoursWeekend,
+              backgroundColor: '#FFFFFF'
+            }
+          ]
+        }
+      }
+    },
+    mounted () {
+      this.renderChart(this.chartData, this.options)
+    }
+  })
+
+</script>
--- src/components/serie/SerieChart.vue
+++ src/components/serie/SerieChart.vue
@@ -12,6 +12,11 @@
             <h2>Class played against</h2>
             <class-against-graph :height="100" />
         </div>
+        <div class="chart">
+            <h2>Win percent by hour</h2>
+            <p>Point size represents number of games</p>
+            <win-percent-by-hour-graph :height="100" />
+        </div>
 
     </div>
 </template>
@@ -22,9 +27,10 @@
   import SerieStarsEvolGraph from '@/components/charts/SerieStarsEvolGraph'
   import SerieRankEvolGraph from '@/components/charts/SerieRankEvolGraph'
   import ClassAgainstGraph from '@/components/charts/ClassAgainstGraph'
+  import WinPercentByHourGraph from '@/components/charts/WinPercentByHourGraph'
 
   export default {
-    components: {SerieStarsEvolGraph, SerieRankEvolGraph, ClassAgainstGraph}
+    components: {SerieStarsEvolGraph, SerieRankEvolGraph, ClassAgainstGraph, WinPercentByHourGraph}
   }
 
 </script>
