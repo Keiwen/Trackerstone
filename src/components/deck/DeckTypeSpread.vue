e548b43af25a6e74a0f4f982fcbef9389c503f46
Keiwen <keiwen.mk@gmail.com>
2018-01-04 12:03:15 +0100

    include classPick in typePick for sorting

--- src/components/deck/DeckTypeSpread.vue
+++ src/components/deck/DeckTypeSpread.vue
@@ -6,18 +6,12 @@
             </div>
         </div>
 
-        <router-link :to="{ name: 'deckTypesList' }">Manage deck types</router-link>
-
-        <div>
-            <fieldset>
-                <legend>Sort by:</legend>
-                <enhanced-check-radio :label="sortLabels"
-                                      :value="sortValues"
-                                      name="deck_type_sort"
-                                      subClass="primary" :animate="true" :inline="true"
-                                      v-model="sortBy" :rounded="true">
-                </enhanced-check-radio>
-            </fieldset>
+        <hr/>
+        <div class="col-xs-6">
+            <class-pick @pick-class="pickClass" :allowNone="true" />
+        </div>
+        <div class="col-xs-6">
+            <router-link :to="{ name: 'deckTypesList' }">Manage deck types</router-link>
         </div>
 
     </div>
@@ -27,35 +21,17 @@
 <script>
   import { mapGetters } from 'vuex'
   import DeckTypeShowSimple from './DeckTypeShowSimple'
-  import { EnhancedCheckRadio } from 'vue-enhanced-check'
+  import ClassPick from '@/components/hsClass/ClassPick'
 
   export default {
-    components: {EnhancedCheckRadio, DeckTypeShowSimple},
+    components: {DeckTypeShowSimple, ClassPick},
     data () {
       return {
         sortBy: ''
       }
     },
     computed: {
-      ...mapGetters(['classes', 'generateTypeTitle', 'getTypesWithClass', 'getTypesOnTop']),
-      sortLabels () {
-        let labels = ['All']
-        for (let hsClass in this.classes) {
-          if (this.classes.hasOwnProperty(hsClass) && hsClass !== '') {
-            labels.push(this.classes[hsClass].name)
-          }
-        }
-        return labels
-      },
-      sortValues () {
-        let values = ['']
-        for (let hsClass in this.classes) {
-          if (this.classes.hasOwnProperty(hsClass) && hsClass !== '') {
-            values.push(this.classes[hsClass].id)
-          }
-        }
-        return values
-      },
+      ...mapGetters(['generateTypeTitle', 'getTypesWithClass', 'getTypesOnTop']),
       typesFiltered () {
         if (this.sortBy === '') {
           const typesTop = this.getTypesOnTop()
@@ -66,6 +42,9 @@
       }
     },
     methods: {
+      pickClass (key) {
+        this.sortBy = key
+      },
       pickType (key) {
         this.$emit('pick-type', key)
       }
