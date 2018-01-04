ddb37007ccf6c51ea8548210a81df49d8a48d115
Keiwen <keiwen.mk@gmail.com>
2018-01-04 11:37:43 +0100

    add class cards for none and DK

--- src/components/hsClass/ClassCard.vue
+++ src/components/hsClass/ClassCard.vue
@@ -26,45 +26,53 @@
 <style lang="scss">
 @import '../../assets/scss/_import.scss';
 
-    .hero {
-        // background: url('../../assets/classes_sprite.png') 0 0 no-repeat;
-        background: url('../../assets/icones_classes.png') 0 0 no-repeat;
-    }
+.hero {
+  // background: url('../../assets/classes_sprite.png') 0 0 no-repeat;
+  background: url('../../assets/icones_classes.png') 0 0 no-repeat;
+}
 
-    .hero-warrior {
-        background-position: -160px -280px;
-    }
+.hero-warrior {
+  background-position: -160px -280px;
+}
 
-    .hero-shaman {
-        background-position: -280px -40px;
-    }
+.hero-shaman {
+  background-position: -280px -40px;
+}
 
-    .hero-rogue {
-        background-position: -280px -280px;
-    }
+.hero-rogue {
+  background-position: -280px -280px;
+}
 
-    .hero-paladin {
-        background-position: -280px -160px;
-    }
+.hero-paladin {
+  background-position: -280px -160px;
+}
 
-    .hero-hunter {
-        background-position: -40px -40px;
-    }
+.hero-hunter {
+  background-position: -40px -40px;
+}
 
-    .hero-druid {
-        background-position: -160px -160px;
-    }
+.hero-druid {
+  background-position: -160px -160px;
+}
 
-    .hero-warlock {
-        background-position: -160px -40px;
-    }
+.hero-warlock {
+  background-position: -160px -40px;
+}
 
-    .hero-mage {
-        background-position: -40px -160px;
-    }
+.hero-mage {
+  background-position: -40px -160px;
+}
 
-    .hero-priest {
-        background-position: -40px -280px;
-    }
+.hero-priest {
+  background-position: -40px -280px;
+}
+
+.hero- {
+  background-position: -40px -400px;
+}
+
+.hero-dk {
+  background-position: -160px -400px;
+}
 
 </style>
