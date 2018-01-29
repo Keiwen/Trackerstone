dfb6fc08ab613eabfb6dece6abaa92303b074768
Keiwen <keiwen.mk@gmail.com>
2018-01-29 11:44:33 +0100

    tip arena reward

--- src/components/tips/TipsMain.vue
+++ src/components/tips/TipsMain.vue
@@ -10,6 +10,10 @@
                 <h3>Percentage of arena runs reaching wins</h3>
                 <tip-arena-run-percentage></tip-arena-run-percentage>
             </div>
+            <div class="carousel-cell tip">
+                <h3>Arena rewards</h3>
+                <tip-arena-reward></tip-arena-reward>
+            </div>
         </flickity>
 
     </div>
@@ -20,8 +24,9 @@
   import Flickity from 'vue-flickity'
   import TipCardProbability from '@/components/tips/TipCardProbability'
   import TipArenaRunPercentage from '@/components/tips/TipArenaRunPercentage'
+  import TipArenaReward from '@/components/tips/TipArenaReward'
 
   export default {
-    components: {Flickity, TipCardProbability, TipArenaRunPercentage}
+    components: {Flickity, TipCardProbability, TipArenaRunPercentage, TipArenaReward}
   }
 </script>
