7afe18acf349b947f7554014227fcab6fd6bc86c
Keiwen <keiwen.mk@gmail.com>
2018-08-21 12:07:39 +0200

    new component to choose heroes

---
+++ src/components/settings/MyHeroes.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container-fluid">
+        <h2>My Heroes</h2>
+
+        <div class="row myHeroes">
+            <div class="row" v-for="(hsClass, key) in classes" v-if="key">
+                <div class="col-xs-3 col-md-2">
+                    <class-card :hsClass="hsClass" />
+                </div>
+                <div class="col-xs-7 col-xs-offset-1 col-md-offset-2 myHeroesPicker">
+                    <enhanced-check-radio :label="getHeroesDisplayForClass(key)"
+                                          :value="heroesForClass(key)"
+                                          :name="key + '_hero_pick'"
+                                          v-model="heroPick[key]"
+                                          @input="chooseHeroForClass(key)"
+                                          subClass="default" :animate="true" :inline="true" :rounded="true">
+                    </enhanced-check-radio>
+                </div>
+            </div>
+            <div class="col-xs-1">
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+<script>
+  import { mapGetters } from 'vuex'
+  import * as storeMut from '@/store/mutation-types'
+  import { EnhancedCheckRadio } from 'vue-enhanced-check'
+  import ClassCard from '../hsClass/ClassCard'
+
+  export default {
+    components: {ClassCard, EnhancedCheckRadio},
+    data () {
+      return {
+        heroPick: {}
+      }
+    },
+    computed: {
+      ...mapGetters(['classes', 'className', 'myHeroes', 'heroesForClass'])
+    },
+    mounted () {
+      this.heroPick = JSON.parse(JSON.stringify(this.myHeroes))
+    },
+    methods: {
+      getHeroesDisplayForClass (classKey) {
+        let heroes = JSON.parse(JSON.stringify(this.heroesForClass(classKey)))
+        for (let i = 0; i < heroes.length; i++) {
+          heroes[i] = heroes[i].charAt(0).toUpperCase() + heroes[i].slice(1)
+        }
+        return heroes
+      },
+      chooseHeroForClass (hsClass) {
+        this.$store.commit(storeMut.CHOOSE_HERO, {
+          hsClass: hsClass,
+          hero: this.heroPick[hsClass]
+        })
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+    .myHeroesPicker {
+        text-align: justify;
+    }
+</style>
