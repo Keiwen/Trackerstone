6b6394afb3e2354ece0f081b8610fb0c39b808b6
Keiwen <keiwen.mk@gmail.com>
2023-01-11 23:46:16 +0100

    add chest reward icons

--- src/components/serie/ChestHome.vue
+++ src/components/serie/ChestHome.vue
@@ -3,7 +3,6 @@
         <h2>Current chest</h2>
         <div class="row">
             <div class="col-xs-9" :class="'serieChest serieChest-' + currentChest.rank">
-                {{ currentChest.level }}
             </div>
             <div @click="showChestNote()" class="col-xs-3">
                 <icon-info :noted="true"></icon-info>
@@ -81,7 +80,42 @@
 <style lang="scss" scoped>
 
     .serieChest {
-        height: 65px;
+        width: 110px;
+        height: 80px;
+        display: inline-block;
+        background: url('../../assets/chest_sprite.png') 0 0 no-repeat;
+        background-position: 0 0;
+        margin-left: 80px;
+        &.serieChest-45 {
+          background-position: 0 -80px;
+        }
+        &.serieChest-40 {
+          background-position: 0 -160px;
+        }
+        &.serieChest-35 {
+          background-position: 0 -240px;
+        }
+        &.serieChest-30 {
+          background-position: 0 -320px;
+        }
+        &.serieChest-25 {
+          background-position: 0 -400px;
+        }
+        &.serieChest-20 {
+          background-position: 0 -480px;
+        }
+        &.serieChest-15 {
+          background-position: 0 -560px;
+        }
+        &.serieChest-10 {
+          background-position: 0 -640px;
+        }
+        &.serieChest-5 {
+          background-position: 0 -720px;
+        }
+        &.serieChest-0 {
+          background-position: 0 -800px;
+        }
     }
 
     .reward-container {
