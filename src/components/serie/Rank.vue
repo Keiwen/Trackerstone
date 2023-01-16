c898e3a87ec4821cbde61f3ae49dc0fa924200aa
Keiwen <keiwen.mk@gmail.com>
2023-01-16 20:17:43 +0100

    add rank sprite and manage icons

--- src/components/serie/Rank.vue
+++ src/components/serie/Rank.vue
@@ -47,87 +47,165 @@
         .rankbadge {
             display: block;
             position: relative;
-            //background: url('../../assets/spriteHS.png') 0 0 no-repeat;
-            width: 105px;
-            height: 120px;
-            background-size: 600px 600px;
+            background: url('../../assets/ranks_sprite.png') 0 0 no-repeat;
+            width: 80px;
+            height: 100px;
+            margin-left: 50px;
+            &.rank-50 {
+              background-position: 0 0;
+            }
+            &.rank-49 {
+              background-position: 0 -100px;
+            }
+            &.rank-48 {
+              background-position: 0 -200px;
+            }
+            &.rank-47 {
+              background-position: 0 -300px;
+            }
+            &.rank-46 {
+              background-position: 0 -400px;
+            }
+            &.rank-45 {
+              background-position: 0 -500px;
+            }
+            &.rank-44 {
+              background-position: 0 -600px;
+            }
+            &.rank-43 {
+              background-position: 0 -700px;
+            }
+            &.rank-42 {
+              background-position: 0 -800px;
+            }
+            &.rank-41 {
+              background-position: 0 -900px;
+            }
+            &.rank-40 {
+              background-position: -80px 0;
+            }
+            &.rank-39 {
+              background-position: -80px -100px;
+            }
+            &.rank-38 {
+              background-position: -80px -200px;
+            }
+            &.rank-37 {
+              background-position: -80px -300px;
+            }
+            &.rank-36 {
+              background-position: -80px -400px;
+            }
+            &.rank-35 {
+              background-position: -80px -500px;
+            }
+            &.rank-34 {
+              background-position: -80px -600px;
+            }
+            &.rank-33 {
+              background-position: -80px -700px;
+            }
+            &.rank-32 {
+              background-position: -80px -800px;
+            }
+            &.rank-31 {
+              background-position: -80px -900px;
+            }
+            &.rank-30 {
+              background-position: -160px 0;
+            }
+            &.rank-29 {
+              background-position: -160px -100px;
+            }
+            &.rank-28 {
+              background-position: -160px -200px;
+            }
+            &.rank-27 {
+              background-position: -160px -300px;
+            }
+            &.rank-26 {
+              background-position: -160px -400px;
+            }
+            &.rank-25 {
+              background-position: -160px -500px;
+            }
+            &.rank-24 {
+              background-position: -160px -600px;
+            }
+            &.rank-23 {
+              background-position: -160px -700px;
+            }
+            &.rank-22 {
+              background-position: -160px -800px;
+            }
+            &.rank-21 {
+              background-position: -160px -900px;
+            }
+            &.rank-20 {
+              background-position: -240px 0;
+            }
+            &.rank-19 {
+              background-position: -240px -100px;
+            }
+            &.rank-18 {
+              background-position: -240px -200px;
+            }
+            &.rank-17 {
+              background-position: -240px -300px;
+            }
+            &.rank-16 {
+              background-position: -240px -400px;
+            }
+            &.rank-15 {
+              background-position: -240px -500px;
+            }
+            &.rank-14 {
+              background-position: -240px -600px;
+            }
+            &.rank-13 {
+              background-position: -240px -700px;
+            }
+            &.rank-12 {
+              background-position: -240px -800px;
+            }
+            &.rank-11 {
+              background-position: -240px -900px;
+            }
+            &.rank-10 {
+              background-position: -320px 0;
+            }
+            &.rank-9 {
+              background-position: -320px -100px;
+            }
+            &.rank-8 {
+              background-position: -320px -200px;
+            }
+            &.rank-7 {
+              background-position: -320px -300px;
+            }
+            &.rank-6 {
+              background-position: -320px -400px;
+            }
+            &.rank-5 {
+              background-position: -320px -500px;
+            }
+            &.rank-4 {
+              background-position: -320px -600px;
+            }
+            &.rank-3 {
+              background-position: -320px -700px;
+            }
+            &.rank-2 {
+              background-position: -320px -800px;
+            }
+            &.rank-1 {
+              background-position: -320px -900px;
+            }
+            &.rank-0 {
+              background-position: -400px 0;
+            }
         }
 
-        .rank-25 {
-            background-position: 0 0;
-        }
-        .rank-24 {
-            background-position: -120px 0;
-        }
-        .rank-23 {
-            background-position: -240px 0;
-        }
-        .rank-22 {
-            background-position: -360px 0;
-        }
-        .rank-21 {
-            background-position: -480px 0;
-        }
-        .rank-20 {
-            background-position: 0 -120px;
-        }
-        .rank-19 {
-            background-position: -120px -120px;
-        }
-        .rank-18 {
-            background-position: -240px -120px;
-        }
-        .rank-17 {
-            background-position: -360px -120px;
-        }
-        .rank-16 {
-            background-position: -480px -120px;
-        }
-        .rank-15 {
-            background-position: 0 -240px;
-        }
-        .rank-14 {
-            background-position: -120px -240px;
-        }
-        .rank-13 {
-            background-position: -240px -240px;
-        }
-        .rank-12 {
-            background-position: -360px -240px;
-        }
-        .rank-11 {
-            background-position: -480px -240px;
-        }
-        .rank-10 {
-            background-position: 0 -360px;
-        }
-        .rank-9 {
-            background-position: -120px -360px;
-        }
-        .rank-8 {
-            background-position: -240px -360px;
-        }
-        .rank-7 {
-            background-position: -360px -360px;
-        }
-        .rank-6 {
-            background-position: -480px -360px;
-        }
-        .rank-5 {
-            background-position: 0 -480px;
-        }
-        .rank-4 {
-            background-position: -120px -480px;
-        }
-        .rank-3 {
-            background-position: -240px -480px;
-        }
-        .rank-2 {
-            background-position: -360px -480px;
-        }
-        .rank-1 {
-            background-position: -480px -480px;
-        }
 
         .serieStar {
             padding: 15px 0 15px 0;
